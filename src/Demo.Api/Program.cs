b827ea143d739872533e5267a507ae4fb60e17c1
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2024-04-25 14:28:11 +0200

    Add Swagger

--- src/Demo.Api/Program.cs
+++ src/Demo.Api/Program.cs
@@ -1,11 +1,33 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Swagger
+var x = $"{Assembly.GetCallingAssembly().GetName().Name}.xml";
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Demo API", Version = "v1" });
+
+    // Include XML comments to Swagger documentation
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, x));
+
+    // Remove trailing "Dto" from schema names
+    c.CustomSchemaIds(x => x.Name[..x.Name.LastIndexOf("Dto")]);
+
+    // Add Operation ID based on controller method name and remove trailing "Async"
+    c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"]}"[..$"{e.ActionDescriptor.RouteValues["action"]}".LastIndexOf("Async")]);
+});
+
+// Skip rest of setup, if app is executed in context of dotnet-swagger
+if (Assembly.GetEntryAssembly()?.FullName?.Contains("dotnet-swagger") == true)
+{
+    return;
+}
 
 var app = builder.Build();
 
